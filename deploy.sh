ee1503450cf317ba9e0320c6022b51334216f8e7
Kevin Kuchta <kevin@kevinkuchta.com>
2018-02-12 16:05:15 -0800

    Update list.js url and add a profile to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 cp index.html s3://archive.pickaxe.club/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region=us-east-1
+aws s3 cp index.html s3://archive.pickaxe.club/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region=us-east-1 --profile=pickaxe_s3
